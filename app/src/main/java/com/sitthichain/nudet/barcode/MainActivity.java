ef86cf85beb8d5b0b0a7a2ab3d300ebb141f7a89
sitthichain <sitthichai.n@gmail.com>
2016-11-22 16:25:24 +0700

    Create Image message successful.

--- app/src/main/java/com/sitthichain/nudet/barcode/MainActivity.java
+++ app/src/main/java/com/sitthichain/nudet/barcode/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.sitthichain.nudet.barcode.MESSAGE";
@@ -17,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
     public void sendDocument(View view) {
         //Intent intent = new Intent(this, DisplayMessageActivity.class);
         //Toast.makeText(DisplayMessageActivity.class, "Hello", Toast.LENGTH_LONG).show();
+
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextBarcode);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        //EditText editText = (EditText) findViewById(R.id.editTextBarcode);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+
+
         startActivity(intent);
 
     }
