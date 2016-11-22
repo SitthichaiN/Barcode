922c7f5824667d5360acb2366ac1eefba8799642
sitthichain <sitthichai.n@gmail.com>
2016-11-22 13:03:48 +0700

    Create message successful.

---
+++ app/src/main/java/com/sitthichain/nudet/barcode/MainActivity.java
@@ -0,0 +1,27 @@
+package com.sitthichain.nudet.barcode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.sitthichain.nudet.barcode.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendDocument(View view) {
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //Toast.makeText(DisplayMessageActivity.class, "Hello", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextBarcode);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
