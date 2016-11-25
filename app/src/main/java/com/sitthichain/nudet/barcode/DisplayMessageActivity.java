9a229fd2de9ad46eddbffd89d8128f70e7686e8b
sitthichain <sitthichai.n@gmail.com>
2016-11-25 17:28:30 +0700

    Display image from WCF  Service successful.

--- app/src/main/java/com/sitthichain/nudet/barcode/DisplayMessageActivity.java
+++ app/src/main/java/com/sitthichain/nudet/barcode/DisplayMessageActivity.java
@@ -2,13 +2,19 @@ package com.sitthichain.nudet.barcode;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ContentFrameLayout;
+import android.util.Base64;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
@@ -17,6 +23,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    private Bitmap Image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +44,18 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Start call web service...
         GetVehicleDocument getVehicleDocument = new GetVehicleDocument(DisplayMessageActivity.this);
         getVehicleDocument.execute(documentString);
+
         // End call web service.
-        ImageView imageView = new ImageView(this);
-        imageView.setImageResource(R.drawable.man);
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
-        layout.addView(imageView);
+
+        //ImageView imageView = new ImageView(this);
+        //imageView.setImageBitmap(returnBitmap);
+        //imageView.setImageResource(R.drawable.man);
+        //ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        //layout.addView(imageView);
     }   // OnCreate
 
-    private class GetVehicleDocument extends AsyncTask<String, Void, String> {
-        private final static String SERVICE_URI = "http://58.137.117.77:8022/ServicePerson.svc/data/";
+    private class GetVehicleDocument extends AsyncTask<String, Void, Bitmap> {
+        private final static String SERVICE_URI = "http://172.28.52.16:8022/ServiceDocuments.svc/GetDocumentById/";
         private String returnString = "";
         private Context context;
 
@@ -54,8 +64,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(String... params) {
-
+        protected Bitmap doInBackground(String... params) {
+            Bitmap retBitmap = null;
             Log.d("Log_V1 > ", "Params : " + params[0]);
 
             OkHttpClient httpClient = new OkHttpClient();
@@ -68,19 +78,40 @@ public class DisplayMessageActivity extends AppCompatActivity {
             Log.d("Log_V1 > ", "checking");
             try {
                 Response response = httpClient.newCall(request).execute();
-                returnString = response.body().string();
+                //returnString = response.body().string();
+
+                String jsonString = response.body().string();
+                JSONObject jsonObject = new JSONObject(jsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("GetDocumentByIdResult");
+                JSONObject jsonObjectReturn = jsonArray.getJSONObject(0);
+
+                String docu = jsonObjectReturn.getString("DocumentFile");
+                int docLenght = Integer.parseInt(jsonObjectReturn.getString("Lenght")) ;
+
+                byte[] bytes = Base64.decode(docu, Base64.DEFAULT);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, docLenght);
+                retBitmap = bitmap;
+
                 // Do something with the response.
-                Log.d("Log_V1 > ","Response : " + returnString);
+                Log.d("Log_V1 > ","Response : " + docLenght);
             } catch (Exception e) {
                 e.printStackTrace();
+                Log.d("Log_V1 > ","Error : " + e.toString());
             }
 
-            return returnString;
+            return retBitmap;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(Bitmap result) {
+            //super.onPostExecute(result);
+
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView3);
+            imageView.setImageBitmap(result);
+            //imageView.setImageResource(R.drawable.man);
+            //ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+            //layout.addView(imageView);
         }
     }
 
